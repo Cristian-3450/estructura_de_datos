7c7c39b11d60cd658cb44b245b21aedf460d22e0
Cristian-3450 <99370184+Cristian-3450@users.noreply.github.com>
2022-03-30 20:12:53 -0400

    Add files via upload

--- PILAS Y COLAS/ejemplo_pilas_y_colas_con_clases.py
+++ PILAS Y COLAS/ejemplo_pilas_y_colas_con_clases.py
@@ -11,11 +11,27 @@ class stack:
         return slef.items[len(self.items)-1]
     def size(self):
         return len(self.items)
-s=stack() #creando un objeto de la clase stack
-print(s.estaVacio())  #muestra TRUE o FALSE
-s.push(100)  #adiciona un valor
-s.push(200)  #adiciona un valor
-s.pop() #saca un valor (200)
-print(s.size()) #1
-
+s=stack() # creando un objeto de la clase stack
+# print(s.estaVacio())  # muestra TRUE o FALSE
+# s.push(100)  # adiciona un valor
+# s.push(200)  # adiciona un valor
+# s.pop() # saca un valor (200)
+# print(s.size()) # 1
+#print(s.estaVacio())  # muestra TRUE o FALSE
+n=int(input("ingrese la cantidad de elementos que desea ingresar: "))
+for i in range(0,n,1):
+    e=input("ingrese el elemento: ")
+    s.push(e)
+print(s.size())
+p=input("desea sacar un elemento(s/n):")
+while p=='s' or p=='S':
+    if s.estaVacio() != True:
+        s.pop()
+        print("Tamaño: ",s.size())
+        p=input("desea sacar otro elemento(s/n):")
+    else:
+        print("La pila esta vacia no se puede sacar mas elementos")
+        p='n'
+        
+    
     
